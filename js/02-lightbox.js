284196e258cd6a43edb17c3d3644daa0b8b64ebc
albiorixUA <steblynamaxim@gmail.com>
2021-12-08 23:04:59 +0200

    add solution task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsContainer = document.querySelector(".gallery");
+const makeGalleryMarkup = (gallery) => {
+  const { preview, original, description } = gallery;
+  return `
+    <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+};
+
+const galleryMarkup = galleryItems.map(makeGalleryMarkup).join("");
+galleryItemsContainer.innerHTML = galleryMarkup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
